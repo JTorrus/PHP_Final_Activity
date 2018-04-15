360100da3f96b32c2242e30df13bdf806be6979f
JTorrus <jtorrusgalindo@gmail.com>
2018-04-15 02:52:26 +0200

    Activity 1 finished, index created

--- php/ejercicio_1.php
+++ php/ejercicio_1.php
@@ -1,2 +1,8 @@
 <?php
-
+header('Content-Type:text/xml');
+$xmlFile = simplexml_load_file("../xml/rssfeed.xml");
+if (!empty($xmlFile)) {
+    echo $xmlFile->asXML();
+} else {
+    echo "There was an error loading the XML file";
+}
